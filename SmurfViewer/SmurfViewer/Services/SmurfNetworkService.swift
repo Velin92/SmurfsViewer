0094ed43d09c9fd2ae35d920fb4d24ff05af8482
Mauro Romito <maurovelin@yahoo.com>
2020-07-12 23:44:12 +0200

    some improvements for the viewmodels

---
+++ SmurfViewer/SmurfViewer/Services/SmurfNetworkService.swift
@@ -0,0 +1,9 @@
+//
+//  SmurfNetworkService.swift
+//  SmurfViewer
+//
+//  Created by Mauro Romito on 12/07/2020.
+//  Copyright © 2020 Bofin Ltd. All rights reserved.
+//
+
+import Foundation
--- SmurfViewer/SmurfViewer/ViewModels/SmurfCellViewModel.swift
+++ SmurfViewer/SmurfViewer/ViewModels/SmurfCellViewModel.swift
@@ -10,4 +10,13 @@ import Foundation
 
 class SmurfCellViewModel: Identifiable {
     
+    let name: String
+    let description: String?
+    
+    init?(from model: Smurf) {
+        //the name is required to be displayed in the list
+        guard let name = model.name else{ return nil}
+        self.name = name
+        self.description = model.wikipediaInfo
+    }
 }
